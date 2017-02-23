ad9524d6975d9a39b2c3eaf2a7da7c5e530fc816
Shawn Rummel <shawn.rummel@gmail.com>
2017-02-23 13:12:14 -0600

    testing output

--- src/com/servicemaster/devops/Pipeline.groovy
+++ src/com/servicemaster/devops/Pipeline.groovy
@@ -7,14 +7,25 @@ package com.servicemaster.devops
 class Pipeline implements Serializable {
 
   def env = System.getenv()
+  def script
+
+  Pipeline(def script){
+    this.script = script
+  }
 
   // Environment Variables from PAC.json
   def SolutionPath = 'SVM.Shared.API.Authorizations.sln'
 
   def outputEnvironmentVariables(){
-    env.each{
-      println it
+    env.each{it ->
+      script.echo  it
     }
+  }
+
+  def output(textToOutput){
+    script.echo 'THIS IS THE START OF SOMETHING NEW'
+    script.echo ${textToOutput}
+    script.echo "THIS IS ME HOPING THIS SHIT WORKS ......"
   }
 
   def build() {
